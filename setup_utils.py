a36fe96e5fe73f825b5b702abe674cb3f742377d
loganthomas <logan.thomas005@gmail.com>
2019-11-20 21:57:14 -0600

    add setup_utils.py

---
+++ setup_utils.py
@@ -0,0 +1,85 @@
+"""
+Set up functions
+"""
+# Standard libraries
+import calendar
+from pathlib import Path
+from datetime import datetime
+
+# Third party libraries
+import numpy as np
+
+
+def get_current_year():
+    return datetime.now().year
+
+
+def get_nfl_start_week(curr_year):
+    # Set Sunday as first day of week
+    calendar.setfirstweekday(calendar.SUNDAY)
+
+    # Gather sept days
+    sept = calendar.monthcalendar(curr_year,9)
+    sept = np.array(sept)
+
+    # Get all sept mondays
+    sept_mondays = sept[:,1]
+
+    # Find first mondays date
+    sept_first_monday = min([mon for mon in sept_mondays if mon != 0])
+
+    # Convert to first monday in sept to datetime object
+    sept_first_monday_date = datetime(curr_year,9, sept_first_monday)
+
+    # Find calendar week for start of NFL season
+    nfl_start_cal_week_num = sept_first_monday_date.isocalendar()[1]
+
+    return nfl_start_cal_week_num
+
+
+def get_thanksgiving_week(curr_year):
+    # Set Sunday as first day of week
+    calendar.setfirstweekday(calendar.SUNDAY)
+
+    # Gather nov days
+    nov = calendar.monthcalendar(curr_year,11)
+    nov = np.array(nov)
+
+    # Find 4th thursday in nov
+    nov_thursdays = nov[:,4]
+    thanksgiving_day = [thur for thur in nov_thursdays if thur != 0][3]
+
+    # Convert thanksgiving day to datetime object
+    thanksgiving_day_date = datetime(curr_year,11,thanksgiving_day)
+
+    # Find calendar week for thanksgiving day
+    thanksgiving_cal_week_num = thanksgiving_day_date.isocalendar()[1]
+
+    return thanksgiving_cal_week_num
+
+
+def calculate_nfl_thanksgiving_week(nfl_start_cal_week_num, thanksgiving_cal_week_num):
+    # Find delta between NFL week 1 and Thanksgiving Week
+    delta = thanksgiving_cal_week_num - nfl_start_cal_week_num
+
+    # Add one since indexed at 1
+    pull_week = delta + 1
+
+    return pull_week
+
+
+def create_output_dir(curr_year):
+    """
+    Creates a root directory to house yearly output.
+    Directory name is year the script is run.
+    """
+    # Create a Path object to dir named curr_year
+    output_dir = Path(curr_year)
+
+    # Check if dir exists, if not create
+    if not output_dir.is_dir():
+        output_dir.mkdir()
+
+    return output_dir
+
+
---
+++ tests/test_setup_utils.py
@@ -0,0 +1,87 @@
+# Standard libraries
+import datetime
+import calendar
+
+# Third party libraries
+import pytest
+import numpy as np
+
+# Local libraries
+import setup_utils
+
+# NFL starts week of first Monday of September on Thursday
+nfl_start_dates = [
+    (2010, datetime.datetime(2010, 9, 9)),
+    (2011, datetime.datetime(2011, 9, 8)),
+    (2012, datetime.datetime(2012, 9, 5)),  # Wednesday this year
+    (2013, datetime.datetime(2013, 9, 5)),
+    (2014, datetime.datetime(2014, 9, 4)),
+    (2015, datetime.datetime(2015, 9, 10)),
+    (2016, datetime.datetime(2016, 9, 8)),
+    (2017, datetime.datetime(2017, 9, 7)),
+    (2018, datetime.datetime(2018, 9, 6)),
+    (2019, datetime.datetime(2019, 9, 5)),
+]
+
+
+@pytest.mark.parametrize('year,expected', nfl_start_dates)
+def test_get_nfl_start_week(year, expected):
+    # Setup
+    calendar.setfirstweekday(calendar.SUNDAY)
+    jan = np.array(calendar.monthcalendar(year,1))
+    jan_thursdays = jan[:,4]
+    first_thur_in_jan = [thur for thur in jan_thursdays if thur != 0][0]
+    first_thur_in_jan_date = datetime.datetime(year,1,first_thur_in_jan)
+
+    expected = expected
+
+    # Exercise
+    nfl_start_cal_week_num = setup_utils.get_nfl_start_week(year)
+    week_delta = nfl_start_cal_week_num - 1  # 1-indexed not 0-indexed
+    result = first_thur_in_jan_date + datetime.timedelta(weeks=week_delta)
+
+    # 2012 started on a wednesday
+    if year == 2012:
+        result -= datetime.timedelta(days=1)
+
+    # Verify
+    assert result == expected
+
+    # Cleanup - none necessary
+
+
+thanksgiving_dates = [
+    (2010, datetime.datetime(2010, 11, 25)),
+    (2011, datetime.datetime(2011, 11, 24)),
+    (2012, datetime.datetime(2012, 11, 22)),
+    (2013, datetime.datetime(2013, 11, 28)),
+    (2014, datetime.datetime(2014, 11, 27)),
+    (2015, datetime.datetime(2015, 11, 26)),
+    (2016, datetime.datetime(2016, 11, 24)),
+    (2017, datetime.datetime(2017, 11, 23)),
+    (2018, datetime.datetime(2018, 11, 22)),
+    (2019, datetime.datetime(2019, 11, 28)),
+]
+
+
+@pytest.mark.parametrize('year,expected', thanksgiving_dates)
+def test_get_thanksgiving_week(year, expected):
+    # Setup
+    calendar.setfirstweekday(calendar.SUNDAY)
+    jan = np.array(calendar.monthcalendar(year,1))
+    jan_thursdays = jan[:,4]
+    first_thur_in_jan = [thur for thur in jan_thursdays if thur != 0][0]
+    first_thur_in_jan_date = datetime.datetime(year,1,first_thur_in_jan)
+
+    expected = expected
+
+    # Exercise
+    thanksgiving_cal_week_num = setup_utils.get_thanksgiving_week(year)
+    week_delta = thanksgiving_cal_week_num - 1  # 1-indexed not 0-indexed
+    result = first_thur_in_jan_date + datetime.timedelta(weeks=week_delta)
+
+    # Verify
+    assert result == expected
+
+    # Cleanup - none necessary
+
