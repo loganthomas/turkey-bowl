9832b2c38ace6ac025fb1b196e7c000978602f7a
loganthomas <logan.thomas005@gmail.com>
2019-12-02 21:02:28 -0600

    add better leader board saving

--- utils.py
+++ utils.py
@@ -84,22 +84,75 @@ def create_output_dir(curr_year):
     return output_dir
 
 
-def create_players_df(players):
+def merge_points(participant_teams, players_df):
     """
-    Converts players (list of namedtuples) into pd.DataFrame
+    Notes:
+        Although the 'Bench (RB/WR)' player is reported in the
+        player_stats DF, it is not used to calculate the total points for a
+        given participant. It is assumed that the 'Bench (RB/WR)' player
+        is listed last in the excel spreadsheet.
     """
-    return pd.DataFrame(players)
+    for participant, participant_team in participant_teams.items():
+        merged = pd.merge(
+            participant_team,
+            players_df,
+            left_on=['Player', 'Team'],
+            right_on=['name', 'team'],
+            how='left',
+        )
+
+        merged = merged.drop(['name', 'team',], axis=1)
+        participant_teams[participant] = merged
+
+        # Defensive programming (ignore bench player in case written in as NONE in .xlsx)
+        if sum(pd.isna(merged[:-1]['id'])) > 0:
+            print(f"WARNING!! {participant}'s merge has missing player ids")
+
+    return participant_teams
 
 
-def save_players_df(year, week, output_dir, players_df):
-    players_df_path = output_dir.joinpath(f'yr{year}_wk{week}_player_data.csv')
+def create_leader_board(year, output_dir, participant_teams):
+    """
+    Notes: Bench player assumed last row in excel sheet.
+    Left out of calculation but still shown.
+    """
+    # Instantiate writer to save leader board and data
+    writer = pd.ExcelWriter(output_dir.joinpath(f'{year}_leader_board.xlsx'), engine='xlsxwriter')
 
-    # Sort by actual points and then projected points
-    players_df = players_df.sort_values(['act_pts', 'proj_pts'], ascending=False)
-    players_df = players_df.reset_index(drop=True)
+    # Instantiate leader board data
+    leader_board_data = {}
 
-    # Write to csv
-    players_df.to_csv(players_df_path)
+    for participant, participant_team in participant_teams.items():
+        print(f'\n### {participant.upper()} stats ###\n')
+        print(participant_team)
+        print('\n\n\n')
 
-    return players_df_path
+        # Assumes bench is last row (shown but not included in sum)
+        team_pts = participant_team[:-1]['act_pts'].sum()
+
+        leader_board_data[participant] = team_pts
+
+        # Save to sheet in excel
+        participant_team.to_excel(writer, sheet_name=participant)
+
+    # Create a DataFrame from point_totals
+    leader_board = pd.DataFrame.from_dict(leader_board_data, orient='index', columns=['PTS'])
+
+    # Sort the leader board on highest pts to lowest pts
+    leader_board = leader_board.sort_values('PTS', ascending=False)
+
+    # Create column to show how far ahead each participant is compared to next
+    leader_board['margin'] = leader_board['PTS'].diff(-1)
+
+    # Create column to show how far out of lead they are
+    leader_board['pts_back'] = leader_board.iloc[0,0] - leader_board['PTS'].values
+
+    # Print details on points
+    print(f'\n{leader_board.index[0]} winning with {round(leader_board.iloc[0,0],2)} pts\n')
+    print(leader_board)
+    print('\n')
+
+    # Save leader board as sheet in excel
+    leader_board.to_excel(writer, sheet_name='leader_board')
+    writer.save()
 
