4c1a12a840ebbd3a39f4ca9bae6694c78d3872bd
loganthomas <logan.thomas005@gmail.com>
2020-09-27 15:29:53 -0500

    [ENH] add write_to_json and load_from_json funcs to utils

--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ Utility functions
 # Standard libraries
 import json
 from datetime import datetime
-from typing import Dict
+from pathlib import Path
+from typing import Any, Dict
 
 # Third-party libraries
 import pandas as pd
@@ -14,11 +15,16 @@ def get_current_year() -> int:
     return datetime.now().year
 
 
-def load_stat_ids() -> Dict[int, Dict[str, str]]:
-    with open("stat_ids.json", "r") as stat_ids_file:
-        stat_ids_dict = json.load(stat_ids_file)
+def write_to_json(json_dict: Dict[str, Any], filename: Path) -> None:
+    with open(filename, "w") as json_file:
+        json.dump(json_dict, json_file, indent=2)
 
-    return stat_ids_dict
+
+def load_from_json(filename: Path) -> Dict[str, Any]:
+    with open(filename, "r") as json_file:
+        json_dict = json.load(json_file)
+
+    return json_dict
 
 
 def merge_points(
