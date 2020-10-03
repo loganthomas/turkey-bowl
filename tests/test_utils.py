42feae78f2a1d16135777bd4b0392f1df82c4e64
loganthomas <logan.thomas005@gmail.com>
2020-10-03 08:54:52 -0500

    [TST] add unit tests for utils.write_to_json() and load_from_json()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@
 Unit tests for utils.py
 """
 # Standard libraries
+import json
 from pathlib import Path
 
 # Third-party libraries
@@ -36,26 +37,92 @@ def test_get_current_year(freezer, frozen_date, expected):
     # Cleanup - none necessary
 
 
-def test_write_to_json():
-    pytest.xfail()
+def test_write_to_json(tmp_path):
+    # Setup
+    tmp_file_path = tmp_path.joinpath("test.json")
+    json_data = {"test": "test", "test2": "test2"}
+
+    # Exercise
+    assert tmp_file_path.exists() is False  # non-existent prior to write
+    utils.write_to_json(json_data, tmp_file_path)
+
+    # Verify
+    assert tmp_file_path.exists() is True
+    with open(tmp_file_path, "r") as written_file:
+        result = json.load(written_file)
+
+    assert result == json_data
+
+    # Cleanup - none necessary
 
 
-def test_load_from_json():
-    pytest.xfail()
+def test_load_from_json(tmp_path):
+    # Setup
+    tmp_file_path = tmp_path.joinpath("test.json")
+    json_data = {"test": "test", "test2": "test2"}
+
+    with open(tmp_file_path, "w") as written_file:
+        json.dump(json_data, written_file)
+
+    # Exercise
+    assert tmp_file_path.exists() is True  # existent prior to load
+    result = utils.load_from_json(tmp_file_path)
+
+    # Verify
+    assert result == json_data
+
+    # Cleanup - none necessary
 
 
-# def test_load_stat_ids():
-#     # Setup
-#     file_loc = Path(__file__)
-#     stat_ids_json_path = file_loc.parent.parent.joinpath("stat_ids.json")
+def test_write_to_and_load_from(tmp_path):
+    # Setup
+    tmp_file_path = tmp_path.joinpath("test.json")
+    json_data = {"test": "test", "test2": "test2"}
 
-#     # Exercise
-#     result = utils.load_from_json(stat_ids_json_path)
+    # Exercise
+    assert tmp_file_path.exists() is False  # non-existent prior to write
+    utils.write_to_json(json_data, tmp_file_path)
+    result = utils.load_from_json(tmp_file_path)
 
-#     # Verify
-#     assert len(result) == 91
+    # Verify
+    assert tmp_file_path.exists() is True
+    assert result == json_data
 
-#     for k, v in result.items():
-#         assert int(k) == v["id"]
+    # Cleanup - none necessary
 
-#     # Cleanup - none necessary
+
+def test_load_stat_ids():
+    # Setup
+    file_loc = Path(__file__)
+    stat_ids_json_path = file_loc.parent.parent.joinpath("stat_ids.json")
+
+    # Exercise
+    result = utils.load_from_json(stat_ids_json_path)
+
+    # Verify
+    assert len(result) == 91
+
+    for k, v in result.items():
+        assert int(k) == v["id"]
+        assert list(v.keys()) == ["id", "abbr", "name", "shortName"]
+
+    # Cleanup - none necessary
+
+
+def test_load_player_ids():
+    # Setup
+    file_loc = Path(__file__)
+    stat_ids_json_path = file_loc.parent.parent.joinpath("player_ids.json")
+
+    # Exercise
+    result = utils.load_from_json(stat_ids_json_path)
+
+    # Verify
+    assert "year" in result
+    for k, v in result.items():
+        if k == "year":
+            assert isinstance(v, int)
+        else:
+            assert list(v.keys()) == ["name", "position", "team", "injury"]
+
+    # Cleanup - none necessary
