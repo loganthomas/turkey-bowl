cf10063b2069fcba62ff3e6125c043f42bc2f362
loganthomas <logan.thomas005@gmail.com>
2020-10-06 22:04:11 -0500

    [TST/WIP] working through main function testing

--- tests/test_thanks_fball.py
+++ tests/test_thanks_fball.py
@@ -1,27 +1,199 @@
-# import pytest
-# import utils
-# from draft import Draft
-# from thanks_fball import main
-# from pathlib import Path
-# import mock
+# Standard libraries
+import json
 
-# import functools
+# Third-party libraries
+import pandas as pd
+import pytest
 
-
-def mock_setup(self):
-    self.output_dir = "tmp_archive_year_path"
-    # cls.draft_order_path = tmp_archive_year_path.joinpath('2020_draft_order.json')
-    # cls.draft_sheet_path = tmp_archive_year_path.joinpath('2020_draft_sheet.xlsx')
+# Local libraries
+from draft import Draft
+from thanks_fball import main
 
 
-def test_MockDraft(tmp_path):
+# TODO: update with 2020 week 2
+@pytest.fixture
+def mock_participant_teams():
+    participant_teams = {
+        "Dodd": {
+            "Position": {
+                0: "QB",
+                1: "RB_1",
+                2: "RB_2",
+                3: "WR_1",
+                4: "WR_2",
+                5: "TE",
+                6: "Flex (RB/WR/TE)",
+                7: "K",
+                8: "Defense (Team Name)",
+                9: "Bench (RB/WR/TE)",
+            },
+            "Player": {
+                0: "Josh Allen",
+                1: "David Montgomery",
+                2: "Tarik Cohen",
+                3: "Allen Robinson",
+                4: "Anthony Miller",
+                5: "Dawson Knox",
+                6: "Jared Cook",
+                7: "Cairo Santos",
+                8: "Chicago Bears",
+                9: "Latavius Murray",
+            },
+            "Team": {
+                0: "BUF",
+                1: "CHI",
+                2: "CHI",
+                3: "CHI",
+                4: "CHI",
+                5: "BUF",
+                6: "NO",
+                7: "CHI",
+                8: "CHI",
+                9: "NO",
+            },
+        },
+        "Becca": {
+            "Position": {
+                0: "QB",
+                1: "RB_1",
+                2: "RB_2",
+                3: "WR_1",
+                4: "WR_2",
+                5: "TE",
+                6: "Flex (RB/WR/TE)",
+                7: "K",
+                8: "Defense (Team Name)",
+                9: "Bench (RB/WR/TE)",
+            },
+            "Player": {
+                0: "Dak Prescott",
+                1: "Adrian Peterson",
+                2: "Kerryon Johnson",
+                3: "Marvin Jones",
+                4: "John Brown",
+                5: "T.J. Hockenson",
+                6: "Alvin Kamara",
+                7: "Matt Prater",
+                8: "Detroit Lions",
+                9: "Michael Gallup",
+            },
+            "Team": {
+                0: "DAL",
+                1: "DET",
+                2: "DET",
+                3: "DET",
+                4: "BUF",
+                5: "DET",
+                6: "NO",
+                7: "DET",
+                8: "DET",
+                9: "DAL",
+            },
+        },
+        "Logan": {
+            "Position": {
+                0: "QB",
+                1: "RB_1",
+                2: "RB_2",
+                3: "WR_1",
+                4: "WR_2",
+                5: "TE",
+                6: "Flex (RB/WR/TE)",
+                7: "K",
+                8: "Defense (Team Name)",
+                9: "Bench (RB/WR/TE)",
+            },
+            "Player": {
+                0: "Matt Ryan",
+                1: "D'Andre Swift",
+                2: "Devin Singletary",
+                3: "Russell Gage",
+                4: "Amari Cooper",
+                5: "Jimmy Graham",
+                6: "Michael Thomas",
+                7: "Tyler Bass",
+                8: "Dallas Cowboys",
+                9: "Randall Cobb",
+            },
+            "Team": {
+                0: "ATL",
+                1: "DET",
+                2: "BUF",
+                3: "ATL",
+                4: "DAL",
+                5: "CHI",
+                6: "NO",
+                7: "BUF",
+                8: "DAL",
+                9: "DAL",
+            },
+        },
+    }
+
+    participant_teams = {k: pd.DataFrame(v) for k, v in participant_teams.items()}
+
+    return participant_teams
+
+
+def test_MockDraft(tmp_path, monkeypatch, mock_participant_teams):
+    # Setup
     tmp_archive_path = tmp_path.joinpath("archive")
     tmp_archive_path.mkdir()
 
     tmp_archive_year_path = tmp_path.joinpath("2020")
     tmp_archive_year_path.mkdir()
 
-    assert tmp_archive_year_path == ""
-    # with mock.patch.object(Draft, 'setup', new=mock_setup):
-    #     d = main()
-    # assert d.output_dir == 'testing'
+    participant_teams = mock_participant_teams
+
+    def mock_setup(self):
+        self.output_dir = tmp_archive_year_path
+        self.draft_order_path = tmp_archive_year_path.joinpath("2020_draft_order.json")
+        self.draft_sheet_path = tmp_archive_year_path.joinpath("2020_draft_sheet.xlsx")
+
+        mock_draft_order = {"dodd": 1, "becca": 2, "logan": 3}
+
+        with open(tmp_archive_year_path.joinpath("2020_draft_order.json"), "w") as f:
+            json.dump(mock_draft_order, f)
+
+        with pd.ExcelWriter(
+            tmp_archive_year_path.joinpath("2020_draft_sheet.xlsx")
+        ) as writer:
+            for participant, participant_team in participant_teams.items():
+                participant_team.to_excel(
+                    writer, sheet_name=participant.title(), index=False
+                )
+
+    monkeypatch.setattr("draft.Draft.setup", mock_setup)
+    monkeypatch.setattr("scrape.Scraper.nfl_thanksgiving_calendar_week", 2)
+
+    # Exercise
+    d = main()
+
+    # Verify
+    # assert d.output_dir == tmp_archive_year_path
+    # assert d.draft_order_path == tmp_archive_year_path.joinpath('2020_draft_order.json')
+    # assert d.draft_sheet_path == tmp_archive_year_path.joinpath('2020_draft_sheet.xlsx')
+
+    # assert d.draft_order_path.exists()
+
+    # with open(d.draft_order_path, 'r') as f:
+    #     written_draft_order = json.load(f)
+
+    # assert written_draft_order == {'dodd': 1, 'becca': 2, 'logan': 3}
+
+    # assert d.draft_sheet_path.exists()
+    # written_draft_sheet = pd.read_excel(d.draft_sheet_path, sheet_name=None)
+    # assert list(written_draft_sheet.keys()) == [*(map(str.title, written_draft_order.keys()))]
+
+    # for participant, participant_team in written_draft_sheet.items():
+    #     assert participant_team.equals(participant_teams[participant])
+    # written_draft_sheet = pd.read_excel(tmp_archive_year_path.joinpath('2020_draft_sheet.xlsx'), sheet_name=None)
+
+    # assert list(written_draft_sheet.keys()) == list(participant_teams.keys())
+
+    # for participant, participant_team in d.items():
+    #     assert participant_team.equals(written_draft_sheet[participant])
+
+    # TODO: make a getter and setter for week (make your life easier in future)
+    assert d.nfl_thanksgiving_calendar_week == 2
+    # Cleanup - none necessary
--- thanks_fball.py
+++ thanks_fball.py
@@ -38,58 +38,60 @@ def main():
     # Instantiate Scraper
     scraper = Scraper(year)
 
-    # TODO: projected points only need to be pulled once...
-    # check if df exists (these won't change)
-    projected_player_pts = scraper.get_projected_player_pts()
-    actual_player_pts = scraper.get_actual_player_pts()
+    # # TODO: projected points only need to be pulled once...
+    # # check if df exists (these won't change)
+    # projected_player_pts = scraper.get_projected_player_pts()
+    # actual_player_pts = scraper.get_actual_player_pts()
 
-    # Update player ids (needs to be done once)
-    scraper.update_player_ids(projected_player_pts)
+    # # Update player ids (needs to be done once)
+    # scraper.update_player_ids(projected_player_pts)
 
-    # Create a DataFrame of PROJECTED player pts
-    #   Create/Save if it doesn't already exist
-    #   Load if it already exists
-    week = scraper.nfl_thanksgiving_calendar_week
-    projected_player_pts_df = aggregate.create_player_pts_df(
-        year=year,
-        week=week,
-        player_pts=projected_player_pts,
-        savepath=Path(f"archive/{year}/{year}_{week}_projected_player_pts.csv"),
-    )
+    # # Create a DataFrame of PROJECTED player pts
+    # #   Create/Save if it doesn't already exist
+    # #   Load if it already exists
+    # week = scraper.nfl_thanksgiving_calendar_week
+    # projected_player_pts_df = aggregate.create_player_pts_df(
+    #     year=year,
+    #     week=week,
+    #     player_pts=projected_player_pts,
+    #     savepath=Path(f"archive/{year}/{year}_{week}_projected_player_pts.csv"),
+    # )
 
-    # Create a DataFrame of ACTUAL player pts
-    # This will be created as multiple points in time.
-    # Note: ``actual_player_pts`` will be none until games start
-    if actual_player_pts:
-        actual_player_pts_df = aggregate.create_player_pts_df(
-            year=year, week=week, player_pts=actual_player_pts, savepath=None
-        )
-    else:
-        actual_player_pts_df = projected_player_pts_df[["Player", "Team"]].copy()
-        actual_player_pts_df["ACTUAL_pts"] = 0.0
+    # # Create a DataFrame of ACTUAL player pts
+    # # This will be created as multiple points in time.
+    # # Note: ``actual_player_pts`` will be none until games start
+    # if actual_player_pts:
+    #     actual_player_pts_df = aggregate.create_player_pts_df(
+    #         year=year, week=week, player_pts=actual_player_pts, savepath=None
+    #     )
+    # else:
+    #     actual_player_pts_df = projected_player_pts_df[["Player", "Team"]].copy()
+    #     actual_player_pts_df["ACTUAL_pts"] = 0.0
 
-    # Merge points to teams
-    participant_teams = aggregate.merge_points(
-        participant_teams, projected_player_pts_df
-    )
-    participant_teams = aggregate.merge_points(participant_teams, actual_player_pts_df)
+    # # Merge points to teams
+    # participant_teams = aggregate.merge_points(
+    #     participant_teams, projected_player_pts_df
+    # )
+    # participant_teams = aggregate.merge_points(participant_teams, actual_player_pts_df)
 
-    # Sort robust columns so actual is next to projected
-    participant_teams = aggregate.sort_robust_cols(participant_teams)
+    # # Sort robust columns so actual is next to projected
+    # participant_teams = aggregate.sort_robust_cols(participant_teams)
 
-    # Write robust scores to excel for reviewing if desired
-    aggregate.write_robust_participant_team_scores(
-        year=year,
-        week=week,
-        participant_teams=participant_teams,
-        savepath=Path(
-            f"archive/{year}/{year}_{week}_robust_participant_player_pts.xlsx"
-        ),
-    )
+    # # Write robust scores to excel for reviewing if desired
+    # aggregate.write_robust_participant_team_scores(
+    #     year=year,
+    #     week=week,
+    #     participant_teams=participant_teams,
+    #     savepath=Path(
+    #         f"archive/{year}/{year}_{week}_robust_participant_player_pts.xlsx"
+    #     ),
+    # )
 
-    board = LeaderBoard(year, participant_teams)
-    board.display()
-    board.save()
+    # board = LeaderBoard(year, participant_teams)
+    # board.display()
+    # board.save()
+
+    return scraper
 
 
 if __name__ == "__main__":
